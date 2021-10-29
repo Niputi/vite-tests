295262beb97cae6a3672563316d5684fe6f41c2b
Niputi <7137178+Niputi@users.noreply.github.com>
2021-10-29 22:05:16 +0200

    npm-juice

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{result}}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import juice from 'juice'
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const result = juice('<style>div{color:red;}</style><div>Hello World</div>')
+    return { result }
+  },
+})
+</script>
