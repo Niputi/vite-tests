effd80ef89622444eb94c4ece6d7f337d6057d95
Niputi <7137178+Niputi@users.noreply.github.com>
2021-10-30 19:09:38 +0200

    npm-jotai

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useAtom, atom } from 'jotai'
+import logo from './logo.svg'
+import './App.css'
+
+const countAtom = atom(0)
+
+function App() {
+  const [count, setCount] = useAtom(countAtom)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount(c => c + 1)}>one up <br />
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
